51d85e94c6c1d87b21436e924f3502c43d7ee5ef
Chen <chen.csc@outlook.com>
2023-10-16 21:02:09 -0500

    Added images
    Added four images using data stored in json and fetched the data using javascript. Changed some of the layout in index file.

--- midterm/index.js
+++ midterm/index.js
@@ -3,32 +3,28 @@ fetch("./data.json")       // for reading the json file
 .then(myImages=>loadImages(myImages)); 
 
 function loadImages(myImages)
-{
+{   
     var mainContainer = document.getElementById("images1"); // first to create an container for the object
-    //console.log("hh");  // testing if i can read data from json file 
     let div = document.createElement("div");
     let url = myImages.images[0].url;
     div.innerHTML = `<img src=${url} width="200"></img>`;  
-    //console.log(url);
     mainContainer.appendChild(div);
-    
-    // Create for loop to iterate data in array
-    // for(var i = 0; i < myImages.movies.length; i++)
-    // {
-    //     let title = myImages.images[i].decription;
-    //     //let year = myMovies.images[i].year;
-    //     let url = myImages.images[i].url;  // image 
 
-    //     // print it out to the console --- DOM
-    //     let div = document.createElement("div");
-    //     // make text to the websites
-    //     div.innerHTML = `
-    //     <br>
-    //     ${title} <br>
-    //     <img src=${url} width="200"> </img><br><br>
-    //     `;  
+    var mainContainer2 = document.getElementById("images2");
+    let div2 = document.createElement("div");
+    let url2 = myImages.images[1].url;
+    div2.innerHTML = `<img src=${url2} width="300"></img>`;  
+    mainContainer2.appendChild(div2);
 
-    //     //return back to the maincontainer
-    //     mainContainer.appendChild(div);
-    // }
+    var mainContainer3 = document.getElementById("images3");
+    let div3 = document.createElement("div");
+    let url3 = myImages.images[2].url;
+    div3.innerHTML = `<img src=${url3} width="200"></img>`;  
+    mainContainer3.appendChild(div3);
+
+    var mainContainer4 = document.getElementById("images4");
+    let div4 = document.createElement("div");
+    let url4= myImages.images[3].url;
+    div4.innerHTML = `<img src=${url4} width="200"></img>`;  
+    mainContainer4.appendChild(div4);
 }
